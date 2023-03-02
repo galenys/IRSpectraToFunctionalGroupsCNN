ce6c71ae583ef4df109840abc9ca579c7ca8273d
galenys <shivbhatia10@gmail.com>
2023-03-02 16:57:26 +0000

    Change model, enable GPU use, add test loss after each epoch

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ import pandas as pd
 from model import Model
 from utils import *
 
-NUM_EPOCHS = 100
-BATCH_SIZE = 10
+NUM_EPOCHS = 1000
+BATCH_NUMBER = 10
 
 # batch size, channels, length
 # input_tensor = torch.randn(10, 1, 1000)
 
-model = Model()
-
 dataset = pd.read_csv('dataset.csv').to_numpy()
+
+# CUDA device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Device: {0}'.format(device))
+
+model = Model()
+model.to(device)
 
 # Remove any rows with NaN values
 dataset = dataset[~np.isnan(dataset).any(axis=1)]
@@ -28,16 +33,23 @@ y = torch.from_numpy(y).float()
 x = x.view(-1, 1, 1000)
 y = y.view(-1, 1)
 
-# Normalise the input
-x = (x - x.mean()) / x.std()
-
 # Split the data into test and train (shuffles the data)
 x_test, x_train, y_test, y_train = split_dataset(x, y, test_proportion=0.2)
+
+# Normalise the input
+mu, sigma = x_train.mean(), x_train.std()
+x_train = (x_train - mu) / sigma
+x_test = (x_test - mu) / sigma
 
 critereon = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-batch_split_indices = k_fold_split(BATCH_SIZE, len(x_train))
+batch_split_indices = k_fold_split(BATCH_NUMBER, len(x_train))
+
+x_test = x_test.to(device)
+y_test = y_test.to(device)
+x_train = x_train.to(device)
+y_train = y_train.to(device)
 
 # Test the model before training
 y_pred = model(x_test)
@@ -53,9 +65,12 @@ for epoch in range(NUM_EPOCHS):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print(f'Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}')
-
+    with torch.no_grad():
+        y_pred = model(x_test)        
+        print(f'Epoch: {epoch}, Train loss: {loss.item()}, Test loss: {critereon(y_pred, y_test).item()}')
+ 
 # Test the model after training
 y_pred = model(x_test)
 accuracy = (y_pred.round() == y_test).sum().item() / len(y_test)
 print(f"Accuracy after training: {accuracy}")
+
--- model.py
+++ model.py
@@ -4,24 +4,24 @@ class Model(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=4, kernel_size=5, stride=3),
+            nn.Conv1d(in_channels=1, out_channels=4, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv1d(in_channels=4, out_channels=8, kernel_size=5, stride=3),
+            nn.Conv1d(in_channels=4, out_channels=4, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=5, stride=3),
+            nn.Conv1d(in_channels=4, out_channels=4, kernel_size=3, padding=1),
             nn.ReLU(),
         )
         self.linear_layers = nn.Sequential(
-            nn.Linear(576, 128),
+            nn.Linear(4000, 1000),
             nn.ReLU(),
-            nn.Linear(128, 32),
+            nn.Linear(1000, 128),
             nn.ReLU(),
-            nn.Linear(32, 1),
-            nn.Sigmoid(),
+            nn.Linear(128, 1),
+            nn.ReLU(),
         )
 
     def forward(self, x):
         x = self.conv_layers(x)
-        x = x.view(-1, 576)
+        x = x.view(-1, 4000)
         x = self.linear_layers(x)
         return x
