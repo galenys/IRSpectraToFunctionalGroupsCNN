68b03cded7959e6eb5c533e6faf5d3a4ac712841
Gil Barbosa Ribeiro <gilbarbosaribeiro@gmail.com>
2023-03-14 12:55:58 +0000

    adding comments

--- analysis.py
+++ analysis.py
@@ -3,12 +3,15 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 
+# get values from csv
 y_test = pd.read_csv("y_test.csv").to_numpy()
 y_pred = pd.read_csv("y_pred.csv").to_numpy()
 
+# sklearn shenanigans
 fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
 auc = metrics.auc(fpr, tpr)
 
+# plotting
 plt.plot(fpr, tpr, label = "ROC")
 plt.plot([0, 1], [0, 1], color="darkblue", linestyle="--")
 plt.xlabel("False Positive Rate")
