17d2b35e126619bc793050a34ab47f345e6b99dd
Gil Barbosa Ribeiro <gilbarbosaribeiro@gmail.com>
2023-03-15 12:50:56 +0000

    added f1 score

--- analysis.py
+++ analysis.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn import metrics
 
 # get values from csv
-y_test = pd.read_csv("y_test.csv").to_numpy()
-y_pred = pd.read_csv("y_pred.csv").to_numpy()
+y_test = pd.read_csv("y_test.csv").to_numpy(dtype="float32")
+y_pred = pd.read_csv("y_pred.csv").to_numpy(dtype="float32")
 
 # sklearn shenanigans
 fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
@@ -18,8 +18,9 @@ plt.xlabel("False Positive Rate")
 plt.ylabel("True Positive Rate")
 plt.title("Receiver Operating Characteristic (ROC) Curve, with {0:.3f} AUC".format(auc))
 plt.legend()
-plt.show()
+# plt.show()
 
 # f1 score
-f1 = metrics.f1_score(y_test, y_pred)
+y_pred = y_pred.round()
+f1 = metrics.f1_score(y_test, y_pred, average = "micro")
 print(f"F1 score: {f1}")
